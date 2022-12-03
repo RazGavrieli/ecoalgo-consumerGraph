e21b7c6eeb9f194d0b1d20036a108be6265a1332
Raz <razgavrieli@gmail.com>
2022-12-03 21:28:28 +0200

    shorter code, better docs

--- weak_pareto.py
+++ weak_pareto.py
@@ -1,64 +1,56 @@
 import networkx as nx
 import logging
 e = 0.01
-def find_weak_pareto_improvment(allocation: list[list[float]],valuations: list[list[float]],cycle: list):
+def find_weak_pareto_improvment(allocation: list[list[float]],valuations: list[list[float]],cycle: list = None):
     print_io(allocation, valuations, cycle)
     A, B, consumerGraph = create_consumerGraph(allocation)
-
     changeGraph = create_changeGraph(allocation, valuations, A)
 
     pos = nx.bipartite_layout(consumerGraph, A)
     print_graph(consumerGraph, pos)
     pos = nx.circular_layout(changeGraph)
     print_graph(changeGraph, pos, weight=True)
+    # calculate the product of the given circle (both directions)
     prodOfFirstCircle = 1
     prodOfSecondCircle = 1
     for index in range(0,len(cycle)-2, 2):
-        key = "agent"
         prodOfFirstCircle *= valuations[cycle[index]][cycle[index+1]]/valuations[cycle[index+2]][cycle[index+1]]
         prodOfSecondCircle *= valuations[cycle[len(cycle)-1-index]][cycle[len(cycle)-2-index]]/valuations[cycle[len(cycle)-3-index]][cycle[len(cycle)-2-index]]
 
-    newAllocations = allocation
+    # check the product of the cycles and define parameters for the next algorithm
     if prodOfFirstCircle == 1 and prodOfSecondCircle == 1:
         print("can have weak pareto improvment as both prods are 1")
     elif prodOfFirstCircle < 1:
-        flag = True
-        while flag:
-            rollingProd = 1
-            for index in range(0, len(cycle)-2, 2):
-                agentA = cycle[index]
-                agentB = cycle[index+2]
-                currItem = cycle[index+1]
-
-                currR = valuations[agentA][currItem]/valuations[agentB][currItem]
-                newAllocations[agentA][currItem] -= e/rollingProd
-                if newAllocations[agentA][currItem] < 0:
-                    jump = newAllocations[agentA][currItem]*(-1)
-                    newAllocations[agentB][currItem] += jump
-                    newAllocations[agentA][currItem] = 0
-                    flag = False
-                    break
-                newAllocations[agentB][currItem] += e/rollingProd
-                rollingProd *= currR
+        rangeList = range(0,len(cycle)-2, 2)
+        agentBjump = 2
+        currItemJump = 1
     elif prodOfSecondCircle < 1:
-        flag = True
-        while flag:
-            rollingProd = 1
-            for index in range(len(cycle)-1, 0, -2):
-                agentA = cycle[index]
-                agentB = cycle[index-2]
-                currItem = cycle[index-1]
+        rangeList = range(len(cycle)-1, 0, -2)
+        agentBjump = -2
+        currItemJump = -1
 
-                currR = valuations[agentA][currItem]/valuations[agentB][currItem]
-                newAllocations[agentA][currItem] -= e/rollingProd
-                if newAllocations[agentA][currItem] < 0:
-                    jump = newAllocations[agentA][currItem]*(-1)
-                    newAllocations[agentB][currItem] += jump
-                    newAllocations[agentA][currItem] = 0
-                    flag = False
-                    break
-                newAllocations[agentB][currItem] += e/rollingProd
-                rollingProd *= currR
+    # the algorithm changes the allocations to remove one edge from the graph 
+    newAllocations = allocation
+    flag = True
+    while flag:
+        rollingProd = 1
+        for index in rangeList:
+            agentA = cycle[index]
+            agentB = cycle[index+agentBjump]
+            currItem = cycle[index+currItemJump]
+
+            currR = valuations[agentA][currItem]/valuations[agentB][currItem]
+            
+            if newAllocations[agentA][currItem] - e/rollingProd < 0:
+                jump = newAllocations[agentA][currItem]
+                newAllocations[agentB][currItem] += jump
+                newAllocations[agentA][currItem] = 0
+                flag = False
+                break
+            newAllocations[agentA][currItem] -= e/rollingProd
+            newAllocations[agentB][currItem] += e/rollingProd
+            rollingProd *= currR
+
 
     A, _, improvedConsumerGraph = create_consumerGraph(newAllocations)
     pos = nx.bipartite_layout(improvedConsumerGraph, A)
@@ -73,10 +65,6 @@ def find_weak_pareto_improvment(allocation: list[list[float]],valuations: list[l
     return newAllocations
     
 
-
-
-    
-
 def print_io(
 allocation: list[list[float]] = None,
 valuations: list[list[float]] = None,
@@ -84,7 +72,7 @@ cycle: list = None
 ):
     for i in allocation:
         for j in i:
-            print(j, end=" ")
+            print(j, end="\t")
         print()
     print()
     # for i in valuations:
@@ -142,33 +130,33 @@ def create_changeGraph(allocation: list[list[float]],valuations: list[list[float
     return changeGraph
 
 if __name__ == "__main__":
-    # allocations = [
-    # [0, 1, 0.2, 0.25],
-    # [1, 0, 0.25, 0.25], 
-    # [0, 0, 0.55, 0.5]
-    # ]
-    # valuations = [
-    # [20, 50, 20, 10],
-    # [50, 18, 22, 10],
-    # [15,15,55, 15]
-    # ]
-    # cycle = [0, 2, 1, 3, 2, 2, 0]
     allocations = [
-    [0.9, 0.1, 0],
-    [0.05, 0.9, 0.1] , 
-    [0.05, 0, 0.9]
-     ]
-    valuations = [
-    [3, 1, 6],
-    [6, 3, 1],
-    [1, 6, 3]
+    [0, 1, 0.2, 0.25],
+    [1, 0, 0.25, 0.25], 
+    [0, 0, 0.55, 0.5]
     ]
-    cycle = [0, 0, 1, 2, 2, 0, 0]
+    valuations = [
+    [20, 50, 20, 10],
+    [50, 18, 22, 10],
+    [15,15,55, 15]
+    ]
+    cycle = [0, 2, 1, 3, 2, 2, 0]
+    # allocations = [
+    # [0.9, 0.1, 0],
+    # [0.05, 0.9, 0.1] , 
+    # [0.05, 0, 0.9]
+    #  ]
+    # valuations = [
+    # [3, 1, 6],
+    # [6, 3, 1],
+    # [1, 6, 3]
+    # ]
+    # cycle = [0, 0, 1, 2, 2, 0, 0]
     newAllo = find_weak_pareto_improvment(allocation=allocations, valuations=valuations, cycle=cycle)
     for i in range(len(newAllo[0])):
         sum = 0
         for j in range(len(newAllo)):
             sum += newAllo[j][i]
-        if sum > 1.1 or sum < 0.99:
+        if sum != 1:
             print(i, j, sum, "bad")
     pass
